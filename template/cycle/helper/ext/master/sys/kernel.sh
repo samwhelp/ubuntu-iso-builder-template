56f2ccc4343baa3beb421314bb6e89eb252c7c2b
samwhelp <samwhelp@users.noreply.github.com>
2025-08-22 22:08:51 +0800

    fix

--- template/cycle/helper/ext/master/sys/kernel.sh
+++ template/cycle/helper/ext/master/sys/kernel.sh
@@ -6,12 +6,25 @@
 
 sys_kernel_package_name_find () {
 
+	#sys_kernel_package_name_find_for_specific
+
 	sys_kernel_package_name_find_for_generic
 
 	#sys_kernel_package_name_find_via_apt_cache
 
 	#sys_kernel_package_name_find_via_apt
 
+}
+
+sys_kernel_package_name_find_for_specific () {
+
+	##
+	## apt-cache show linux-image-generic | grep '^Depends'
+	##
+
+	echo "linux-image-6.14.0-27-generic linux-modules-extra-6.14.0-27-generic linux-firmware intel-microcode amd64-microcode"
+
+	return 0
 }
 
 sys_kernel_package_name_find_for_generic () {
